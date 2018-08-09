76e5147564b1f7aa6b08b8299c385941fb7c9d30
Daniel <super.daichao@gmail.com>
2018-08-09 22:15:37 +0800

    [+]增加zidingy自定义注解
    @Action
    @Controller
    @Inject
    @Service

---
+++ kiwi-framework/src/main/java/com/daley/kiwi/annotation/Action.java
@@ -0,0 +1,16 @@
+package com.daley.kiwi.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Daniel
+ * @date 2018/8/9 16:51
+ */
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Action {
+}
---
+++ kiwi-framework/src/main/java/com/daley/kiwi/annotation/Controller.java
@@ -0,0 +1,17 @@
+package com.daley.kiwi.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Daniel
+ * @date 2018/8/9 16:19
+ */
+
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Controller {
+}
