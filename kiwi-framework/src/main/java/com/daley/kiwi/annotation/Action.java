b450cc0fd058d7710fc103d7d0ae9169a4dab80c
Daniel <super.daichao@gmail.com>
2018-08-11 18:18:41 +0800

    [+]initializing HANDLER_MAP
    add ControllerHelper.java

--- kiwi-framework/src/main/java/com/daley/kiwi/annotation/Action.java
+++ kiwi-framework/src/main/java/com/daley/kiwi/annotation/Action.java
@@ -13,4 +13,9 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Action {
+
+    /**
+     * 请求类型与路径
+     */
+    String value();
 }
---
+++ kiwi-framework/src/main/java/com/daley/kiwi/helper/ControllerHelper.java
@@ -0,0 +1,58 @@
+package com.daley.kiwi.helper;
+
+import com.daley.kiwi.annotation.Action;
+import com.daley.kiwi.bean.Handler;
+import com.daley.kiwi.bean.Request;
+import com.daley.kiwi.utils.ClassUtil;
+import com.daley.kiwi.utils.CollectionUtil;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author daley
+ * @date 2018/8/11
+ */
+public final class ControllerHelper {
+    public static final Map<Request, Handler> HANDLER_MAP = new HashMap<>();
+
+    static {
+        //得到所有controller类
+        Set<Class<?>> classSet = ClassLoadHelper.getControllerClassSet();
+        if (CollectionUtil.isNotEmpty(classSet)) {
+            for (Class<?> controllerClass : classSet) {
+                Method[] methods = controllerClass.getDeclaredMethods();
+
+                //找出带@Action的方法
+                if (ArrayUtils.isNotEmpty(methods)) {
+                    for (Method method : methods) {
+                        if (method.isAnnotationPresent(Action.class)) {
+
+                            //@Action 被标注
+                            Action action = method.getDeclaredAnnotation(Action.class);
+                            String mapping = action.value();
+
+                            //验证URL规则
+                            if (mapping.matches("\\w+:/\\w*")) {
+                                String[] array = mapping.split(":");
+                                if (ArrayUtils.isNotEmpty(array) && array.length == 2) {
+                                    String requestMethod = array[0];
+                                    String requestPath = array[1];
+                                    Request request = new Request(requestMethod, requestPath);
+                                    Handler handler = new Handler(controllerClass, method);
+
+                                    //初始化HANDLER_MAP
+                                    HANDLER_MAP.put(request, handler);
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
